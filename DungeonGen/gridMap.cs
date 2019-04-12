33761bda58bfe2ffc4848a23920c8d158529c9cc
Aaron Wiley <manorastroman999@gmail.com>
2019-04-11 20:14:19 -0700

    Generate rooms, Check for room intersections and verify rooms dont generate outside the bounds of the map.

--- DungeonGen/gridMap.cs
+++ DungeonGen/gridMap.cs
@@ -6,8 +6,29 @@ namespace DungeonGen
 {
 public class gridMap
 {
-
-    
+    /* room byte values
+     * 0 = Wall/unaccessable
+     * 1 = Open Floor
+     * 2 = NW corner
+     * 3 = NE corner
+     * 4 = SW corner
+     * 5 = SE corner
+     * 6 = N Wall
+     * 7 = S Wall
+     * 8 = W Wall
+     * 9 = E Wall
+     * 10 = NS corridor
+     * 11 = EW corridor
+     * 12 = N Wall door
+     * 13 = S Wall door
+     * 14 = W Wall door
+     * 15 = E Wall door
+     * 16 = WN corner Coridor
+     * 17 = WS corner coridor
+     * 18 = EN corner coridor
+     * 19 = ES corner coridor
+     * 20 = Dead End coridor
+     */
     private Dictionary<Tuple<int,int>, byte> mapGrid;
     public int dim_x, dim_y, rooms;
     private Random random = new Random();
@@ -48,11 +69,29 @@ public class gridMap
     private List<Tuple<Tuple<int, int>, Tuple<int, int>>> genRooms(int numRms, int minSz, int maxSz)
             {
                 List<Tuple<Tuple<int, int>, Tuple<int, int>>> rmLst = new List<Tuple<Tuple<int, int>, Tuple<int, int>>>();
-                for(int i = 0; i < numRms; i++)
+                Tuple<int, int> anchor;
+                Tuple<int, int> roomSize;
+            for (int i = 0; i < numRms; i++)
                 {
-                Tuple<int, int> roomSize = new Tuple<int, int>(random.Next(minSz, maxSz), random.Next(minSz, maxSz)); 
+                int lim = 0;
+                while(true){
+                    roomSize = new Tuple<int, int>(random.Next(minSz, maxSz), random.Next(minSz, maxSz));
+                    anchor = new Tuple<int, int>(random.Next(1, this.dim_x - 2), random.Next(1, this.dim_y - 2));
+                    if (checkOverlap(anchor, roomSize, rmLst))
+                        {
+                        rmLst.Add(anchor, roomSize);
+                        break;
+                        }
+                    if (lim = 20) break;
+                    lim++;
+                    }               
                 }
             return rmLst;
+            }
+
+    public bool checkOverlap(Tuple<int,int> anch, Tuple<int, int>size, List<Tuple<Tuple<int, int>, Tuple<int, int>>> rLst)
+            {
+            return true;
             }
     
     public void printMap()
